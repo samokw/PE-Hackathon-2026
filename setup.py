faffcb36a97fd77a3e5c1410c3f2890edf07e79f
Samuel Okwusiuno <okwusiunosamuel@gmail.com>
2026-04-03 22:09:04 -0400

    updated setup script to create url table

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from app import create_app
 from app.database import db
+from app.models.urls import Url
 from app.models.users import User
 
 app = create_app()
 
 with app.app_context():
-    db.create_tables([User])
-    # add json logging here 
+    db.create_tables([User, Url])
+    # add json logging here
